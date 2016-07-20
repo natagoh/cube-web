442520be72b0cedf783e722f4003342d7cc8ac28
natagoh <gohnatasha98@gmail.com>
2016-07-19 23:27:39 -0400

    reload on resize

--- main.js
+++ main.js
@@ -14,11 +14,10 @@ $(document).ready(function(){
 })
 
 //ensures accurate info for nav bar stuff
-/*
 $( window ).resize(function() {
     location.reload();
 })
-*/
+
 //navigation bar stuff
 var isNavClosed = false;
 $(document).ready(function(){
