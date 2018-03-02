101a61d6c501d9a838118163c8afdebc0403ca37
natagoh <natagoh@stanford.edu>
2018-03-01 23:18:21 -0800

    base

--- js/script.js
+++ js/script.js
@@ -1,25 +1,33 @@
 // initialize cube
 var cubeSetup = function() {
-	var w = window.innerWidth;
-	var h = window.innerHeight;
+	var w = window.innerWidth/2;
+	var h = window.innerHeight/2;
+	console.log(w + " " + h)
 
-	var offset = h/2;
 
 	// set the perspective
-	document.getElementById("viewbox").style.webkitPerspective = w*4 + "px";
-	document.getElementById("viewbox").style.perspectiveOrigin = w + "px" + "50%";
+
+	// set the scene
+	// document.getElementById("viewbox").style.webkitPerspective = w*4 + "px";
+	// document.getElementById("viewbox").style.perspectiveOrigin = w + "px" + "50%";
+
+	// set perspective
+	document.getElementById("scene").style.width = w + "px";
+	document.getElementById("scene").style.height = h + "px";
 
 	// set the sizes of the cube 
 	document.getElementById("cube").style.height = h + "px";
 	document.getElementById("cube").style.width = w + "px";
+	document.getElementById("cube").style.transform = "perspective(" + w*4 + "px)";
+	document.getElementById("cube").style.webkitTransform = "translateZ(" + (-1*w/2) + "px)";
 
 	// set the size of the faces
 	var faces = document.getElementById("cube").getElementsByClassName('face');
 	for (i = 0; i < faces.length; i++) {
 		faces[i].style.height = h + "px";
 		faces[i].style.width = w + "px";
-		faces[i].style.webkitPerspective = w*4 + "px";
-		faces[i].style.webkitTransform = "transformOrigin(50%, 50%," + -1*offset + "px)";
+		faces[i].style.transform = "perspective(" + w*4 + "px)";
+		faces[i].style.transform = "transformOrigin(50% 50% " + -1*h/2 + "px)";
 	}
 
 }
