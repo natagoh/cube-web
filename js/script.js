b208d0ed9aff8766196f801aa19ab69bc6afe75f
natagoh <natagoh@stanford.edu>
2018-03-01 21:08:00 -0800

    testing cube

--- js/script.js
+++ js/script.js
@@ -39,8 +39,32 @@ function fill() {
 	}
 }
 
+// initialize cube
+var cubeSetup = function() {
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+
+	// set the perspective
+	document.getElementById("viewbox").style.webkitPerspective = w*4 + "px";
+	document.getElementById("viewbox").style.perspectiveOrigin = w + "px" + "50%";
+
+	// set the sizes of the cube 
+	document.getElementById("cube").style.height = h + "px";
+	document.getElementById("cube").style.width = w + "px";
+
+	// set the size of the faces
+	var faces = document.getElementById("cube").getElementsByClassName('face');
+	for (i = 0; i < faces.length; i++) {
+		faces[i].style.height = h + "px";
+		faces[i].style.width = w + "px";
+	}
+
+}
+
 // centering splash logo and intro
 var center = function() {
+	cubeSetup();
+
 	var divHeight = document.getElementById("splash-container").clientHeight;
 	var windowHeight = window.innerHeight;
 	var offset = (windowHeight - divHeight)/2 + "px";
@@ -58,3 +82,26 @@ window.onresize = function(event) {
 	center();
 };
 
+var xAngle = 0, yAngle = 0;
+document.addEventListener('keydown', function(e) {
+  switch(e.keyCode) {
+
+    case 37: // left
+      yAngle -= 90;
+      break;
+
+    case 38: // up
+      xAngle += 90;
+      break;
+
+    case 39: // right
+      yAngle += 90;
+      break;
+
+    case 40: // down
+      xAngle -= 90;
+      break;
+  };
+
+  document.getElementById("cube").style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
+}, false);
